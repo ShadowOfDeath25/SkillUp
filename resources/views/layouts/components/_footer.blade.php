f92c672347da85c73e4ae9a8c3cf1ece80cb9d0d
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2024-12-21 21:05:45 +0200

    Other minor adjustments  (somebody kill me please)

--- resources/views/layouts/components/_footer.blade.php
+++ resources/views/layouts/components/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>Copyright &copy; <span>   The Skillup Team</span></p>
+    <p>Copyright &copy; <span> The Skillup Team</span></p>
     <div class="links">
         <a href="https://github.com/ShadowOfDeath25/skillup" target="_blank"><i class="fa-brands fa-github"></i></a>
     </div>
